672492f2a82463c6ef84c29a2e3f9ea7dbaa4a8f
ImtiazAhmed01 <imtiazahm21@gmail.com>
2024-01-06 14:05:24 +0600

    js file added

---
+++ index.js
@@ -0,0 +1,7 @@
+//card grab
+const cards = document.querySelectorAll(".card");
+// cards.forEach((singleCard) => {
+//     console.log(singleCard[0].childNodes[3]);
+// })
+console.log(cards[0].childNodes)
+const price = cards[0].childNodes[3]
